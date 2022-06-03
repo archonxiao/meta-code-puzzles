e1a0a05a284acb3611d5b53f638b673e059245c1
Tom Xiao <tom.xiao@zerologix.com>
2022-06-03 15:40:09 +1000

    Add solution to the puzzle Cafeteria

--- level1/Cafeteria/Cafeteria.php
+++ level1/Cafeteria/Cafeteria.php
@@ -1,1 +1,17 @@
 <?php
+function getMaxAdditionalDinersCount(int $N, int $K, int $M, array $S): int {
+    // Write your code here
+    $total = 0;
+    sort($S);
+    for ($i = 0; $i < $M; $i++) {
+        if ($i === 0) {
+            $total += floor(($S[$i] - 1) / ($K + 1));
+        } else {
+            $total += floor(($S[$i] - $S[$i - 1]) / ($K + 1)) - 1;
+        }
+        if ($i === ($M - 1)) {
+            $total += floor(($N - $S[$i]) / ($K + 1));
+        }
+    }
+    return $total;
+}
